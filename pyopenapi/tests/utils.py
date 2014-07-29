fbe951506252bffe083942c246736bca14f13a4c
mission.liao <missionaryliao@gmail.com>
2014-07-29 20:41:54 +0800

    continue fixing

--- pyopenapi/tests/utils.py
+++ pyopenapi/tests/utils.py
@@ -6,7 +6,7 @@ def get_test_data_folder(which=""):
     """
     import pyopenapi.tests.data
 
-    folder = os.path.dirname(pyopenapi.tests.data.__file__)
+    folder = os.path.dirname(os.path.abspath(pyopenapi.tests.data.__file__))
     if which != None:
         folder = os.path.join(folder, which)
     return folder
