8a5d73e69ad91180c7f25157caac5dc055ba6d55
mission.liao <missionaryliao@gmail.com>
2014-09-01 16:16:57 +0800

    rename prim.py -> primitives.py

--- pyopenapi/tests/test_model.py
+++ pyopenapi/tests/test_model.py
@@ -1,6 +1,6 @@
 from pyopenapi import SwaggerApp
 from pyopenapi.contrib.client.requests import Client
-from pyopenapi.prim import Model
+from pyopenapi.primitives import Model
 from .utils import get_test_data_folder
 import unittest
 import httpretty
