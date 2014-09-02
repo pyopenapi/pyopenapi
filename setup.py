1c193c319c15dd71ee7cc9cd00ded81857a5eb4d
mission.liao <missionaryliao@gmail.com>
2014-09-02 20:14:27 +0800

    sync version bumping

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
+
+version = open('pyopenapi/VERSION', 'r').readline().strip()
+
 setup(
     name = 'pyopenapi',
     packages = find_packages(exclude=['*.tests.*']),
-    version = '0.0.2',
+    version = version,
     description = 'A type safe Swagger Client',
     author = 'Mission Liao',
     author_email = 'missionaryliao@gmail.com',
     url = 'https://github.com/AntXlab/pyopenapi', # use the URL to the github repo
-    download_url = 'https://github.com/AntXlab/pyopenapi/tarball/0.0.2',
+    download_url = 'https://github.com/AntXlab/pyopenapi/tarball/{0}'.format(version),
     keywords = ['swagger', 'REST'], # arbitrary keywords
     classifiers = [
         'Development Status :: 4 - Beta',
