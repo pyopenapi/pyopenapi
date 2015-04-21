c9300a83a448409e2ba94aee23fa362ef380a052
Mission Liao <missionaryliao@gmail.com>
2015-04-21 18:35:41 +0800

    remove type_hint
    check json/yaml by the first few chars.

--- pyopenapi/tests/test_yaml.py
+++ pyopenapi/tests/test_yaml.py
@@ -1,5 +1,4 @@
 from pyopenapi import SwaggerApp
-from pyopenapi.consts import FILE_TYPE_YAML
 from pyopenapi.scan import Scanner
 from pyopenapi.scanner.v2_0 import YamlFixer
 from pyopenapi.spec.v2_0.objects import Operation
@@ -19,8 +18,7 @@ class YAMLTestCase(unittest.TestCase):
         app_yaml = SwaggerApp.load(get_test_data_folder(
             version='2.0',
             which='yaml',
-            ),
-            type_hint=FILE_TYPE_YAML
+            )
         )
         s = Scanner(app_yaml)
         s.scan(route=[YamlFixer()], root=app_yaml.raw, leaves=[Operation])
