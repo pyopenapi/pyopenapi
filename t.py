0878288b0ca292455265a135d8d0c056919004b3
mission-liao <missionaryliao@gmail.com>
2015-11-27 18:54:32 +0800

    [version]
    - add pyswagger.primitives.Renderer for
    https://github.com/mission-liao/pyswagger/issues/44

---
+++ t.py
@@ -0,0 +1,21 @@
+import unittest
+from pyopenapi.tests.test_render import ObjectTestCase as t
+
+
+if __name__ == '__main__':
+    import logging
+    logger = logging.getLogger('pyopenapi')
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+
+    logger.addHandler(console)
+    logger.setLevel(logging.ERROR)
+
+    unittest.TextTestRunner().run(
+        unittest.makeSuite(t, 'test_')
+    )
+
