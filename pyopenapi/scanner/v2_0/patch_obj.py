84442ac9b998d67bc16888075d2d28f5827ab484
mission.liao <missionaryliao@gmail.com>
2017-11-13 00:26:24 +0800

    Refine PatchObj scanner in Swagger 2.0
    - remove dependency between App
    - some patch is not necessary since our major version is 3.0.0 now

--- pyopenapi/scanner/v2_0/patch_obj.py
+++ pyopenapi/scanner/v2_0/patch_obj.py
@@ -16,14 +16,17 @@ class PatchObject(object):
 
     class Disp(Dispatcher): pass
 
+    def __init__(self, swagger):
+        self._swagger = swagger
+
     @Disp.register([Operation])
     def _operation(self, path, obj, app):
         """
         """
-        if isinstance(app.root, Swagger):
+        if isinstance(self._swagger, Swagger):
             # produces/consumes
-            obj.update_field('produces', app.root.produces if len(obj.produces) == 0 else obj.produces)
-            obj.update_field('consumes', app.root.consumes if len(obj.consumes) == 0 else obj.consumes)
+            obj.update_field('produces', self._swagger.produces if len(obj.produces) == 0 else obj.produces)
+            obj.update_field('consumes', self._swagger.consumes if len(obj.consumes) == 0 else obj.consumes)
 
         # combine parameters from PathItem
         if obj._parent_:
@@ -39,14 +42,19 @@ class PatchObject(object):
                 obj.update_field('parameters', copy.copy(obj._parent_.parameters))
 
         # schemes
-        obj.update_field('cached_schemes', app.schemes if len(obj.schemes) == 0 else obj.schemes)
+        if obj.schemes:
+            obj.update_field('cached_schemes', obj.schemes)
+        elif isinstance(self._swagger, Swagger):
+            obj.update_field('cached_schemes', app.schemes if not self._swagger.schemes else self._swagger.schemes)
+        else:
+            obj.update_field('cached_schemes', app.schemes)
 
         # primitive factory
         setattr(obj, '_prim_factory', app.prim_factory)
 
         # inherit service-wide security requirements
-        if obj.security == None and isinstance(app.root, Swagger):
-            obj.update_field('security', app.root.security)
+        if obj.security == None and isinstance(self._swagger, Swagger):
+            obj.update_field('security', self._swagger.security)
 
         # mime_codec
         setattr(obj, '_mime_codec', app.mime_codec)
@@ -56,16 +64,16 @@ class PatchObject(object):
         """
         """
         k = jp_split(path)[-1] # key to the dict containing PathItem(s)
-        if isinstance(app.root, Swagger):
-            host = app.root.host if app.root.host else six.moves.urllib.parse.urlparse(app.url)[1]
+        if isinstance(self._swagger, Swagger):
+            host = self._swagger.host if self._swagger.host else six.moves.urllib.parse.urlparse(app.url)[1]
             host = host if len(host) > 0 else 'localhost'
             url = six.moves.urllib.parse.urlunparse((
                     '',                            # schema
                     host,                          # netloc
-                    (app.root.basePath or '') + k, # path
+                    (self._swagger.basePath or '') + k, # path
                     '', '', '',                    # param, query, fragment
             ))
-            base_path = app.root.basePath
+            base_path = self._swagger.basePath
         else:
             url = None
             base_path = None
