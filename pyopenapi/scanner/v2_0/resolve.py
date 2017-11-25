42669dbd0e6426cd3969a1eb29155332f3b3c682
mission.liao <missionaryliao@gmail.com>
2017-11-25 10:51:18 +0800

    Resolve '$ref' in Swagger 2.0
    Thoes objects are originally 'merged' in 2.0 but we've
    removed the merging process in 2.0.

--- pyopenapi/scanner/v2_0/resolve.py
+++ pyopenapi/scanner/v2_0/resolve.py
@@ -1,8 +1,47 @@
 from __future__ import absolute_import
 from ...errs import CycleDetectionError
 from ...scan import Dispatcher
-from ...spec.v2_0.parser import SchemaContext
-from ...spec.v2_0.objects import Schema
+from ...spec.v2_0.parser import (
+    SchemaContext,
+    PathItemContext,
+    ParameterContext,
+    ResponseContext,
+    )
+from ...spec.v2_0.objects import (
+    Schema,
+    PathItem,
+    Parameter,
+    Response,
+    )
+
+def _resolve(obj, parser, app, path):
+    if obj.ref_obj:
+        return
+
+    if not obj.normalized_ref:
+        return
+
+    ro = app.resolve(
+        obj.normalized_ref,
+        parser=parser,
+        spec_version='2.0',
+        before_return=None
+    )
+    if not ro:
+        raise ReferenceError(
+            'Unable to resolve: {} in {}'.format(
+                obj.normalized_ref, path
+            )
+        )
+    if ro.__class__ != obj.__class__:
+        raise TypeError(
+            'Referenced Type mismatch: {} in {}'.format(
+                obj.normalized_ref, path
+            )
+        )
+
+    obj.update_field('ref_obj', ro)
+
 
 class Resolve(object):
     """ pre-resolve 'normalized_ref' """
@@ -10,20 +49,18 @@ class Resolve(object):
     class Disp(Dispatcher): pass
 
     @Disp.register([Schema])
-    def _schema(self, _, obj, app):
-        if obj.ref_obj:
-            return
+    def _schema(self, path, obj, app):
+        _resolve(obj, SchemaContext, app, path)
 
-        r = obj.normalized_ref
-        if not r:
-            return
+    @Disp.register([PathItem])
+    def _path_item(self, path, obj, app):
+        _resolve(obj, PathItemContext, app, path)
 
-        ro = app.resolve(r, parser=SchemaContext, spec_version='2.0', before_return=None)
-        if not ro:
-            raise ReferenceError('Unable to resolve: {0}'.format(r))
-        if ro.__class__ != obj.__class__:
-            raise TypeError('Referenced Type mismatch: {0}'.format(r))
+    @Disp.register([Parameter])
+    def _parameter(self, path, obj, app):
+        _resolve(obj, ParameterContext, app, path)
 
-        obj.update_field('ref_obj', ro)
-
+    @Disp.register([Response])
+    def _response(self, path, obj, app):
+        _resolve(obj, ResponseContext, app, path)
 
