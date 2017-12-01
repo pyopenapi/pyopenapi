17046474ca3eb610766b04f0e788738aadfa63be
mission.liao <missionaryliao@gmail.com>
2017-12-01 23:12:19 +0800

    Use base2 on Swagger 1.2

--- pyopenapi/spec/v1_2/objects.py
+++ pyopenapi/spec/v1_2/objects.py
@@ -1,310 +1,359 @@
 from __future__ import absolute_import
-from ..base import BaseObj, FieldMeta, Context
+from ..base2 import Base2, field, child, list_, map_
 import six
 import copy
 
 
-class BaseObj_v1_2(BaseObj):
+class BaseObj_v1_2(Base2):
     __swagger_version__ = '1.2'
 
 
-class Items(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Items(BaseObj_v1_2):
     """ Items Object
     """
-    __swagger_fields__ = {
-        '$ref': None,
-        'type': None,
-        'format': None,
+    __fields__ = {
+        '$ref': dict(builder=field),
+        'type': dict(builder=field),
+        'format': dict(builder=field),
     }
 
-
-class ItemsContext(Context):
-    """ Context of Items Object
-    """
-    __swagger_ref_object__ = Items
+    __renamed__ = {
+        'ref': dict(key='$ref'),
+        'type_': dict(key='type'),
+        'format_': dict(key='format'),
+    }
 
 
 class DataTypeObj(BaseObj_v1_2):
     """ Data Type Fields
     """
-    __swagger_fields__ = {
-        'type': None,
-        '$ref': None,
-        'format': None,
-        'defaultValue': None,
-        'enum': None,
-        'items': None,
-        'minimum': None,
-        'maximum': None,
-        'uniqueItems': None,
+    __fields__ = {
+        'type': dict(builder=field),
+        '$ref': dict(builder=field),
+        'format': dict(builder=field),
+        'defaultValue': dict(builder=field),
+        'enum': dict(builder=field),
+        'minimum': dict(builder=field),
+        'maximum': dict(builder=field),
+        'uniqueItems': dict(builder=field),
     }
 
-    def __init__(self, ctx):
-        # Items Object, too lazy to create a Context for DataTypeObj
-        # to wrap this child.
-        items_data = ctx._obj.get('items', None)
-        if items_data:
-            with ItemsContext(ctx._obj, 'items') as items_ctx:
-                items_ctx.parse(items_data)
-        else:
-            setattr(self, self.get_private_name('items'), None)
+    __children__ = {
+        'items': dict(child_builder=Items),
+    }
 
-        super(DataTypeObj, self).__init__(ctx)
+    __renamed__ = {
+        'type_': dict(key='type'),
+        'ref': dict(key='$ref'),
+        'format_': dict(key='format'),
+        'default_value': dict(key='defaultValue'),
+        'unique_items': dict(key='uniqueItems'),
+    }
 
-class Scope(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+
+class Scope(BaseObj_v1_2):
     """ Scope Object
     """
 
-    __swagger_fields__ = {
-        'scope': None,
-        'description': None,
+    __fields__ = {
+        'scope': dict(builder=field, required=True),
+        'description': dict(builder=field),
     }
 
 
-class LoginEndpoint(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class LoginEndpoint(BaseObj_v1_2):
     """ LoginEndpoint Object
     """
 
-    __swagger_fields__ = {
-        'url': None,
+    __fields__ = {
+        'url': dict(builder=field, required=True),
     }
 
 
-class Implicit(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Implicit(BaseObj_v1_2):
     """ Implicit Object
     """
 
-    __swagger_fields__ = {
-        'loginEndpoint': None,
-        'tokenName': None,
+    __fields__ = {
+        'tokenName': dict(builder=field),
+    }
+
+    __children__ = {
+        'loginEndpoint': dict(child_builder=LoginEndpoint, required=True),
+    }
+
+    __renamed__ = {
+        'token_name': dict(key='tokenName'),
+        'login_endpoint': dict(key='loginEndpoint'),
     }
 
 
-class TokenRequestEndpoint(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class TokenRequestEndpoint(BaseObj_v1_2):
     """ TokenRequestEndpoint Object
     """
 
-    __swagger_fields__ = {
-        'url': None,
-        'clientIdName': None,
-        'clientSecretName': None,
+    __fields__ = {
+        'url': dict(builder=field, required=True),
+        'clientIdName': dict(builder=field),
+        'clientSecretName': dict(builder=field),
+    }
+
+    __renamed__ = {
+        'client_id_name': dict(key='clientIdName'),
+        'client_secret_name': dict(key='clientSecretName'),
     }
 
 
-class TokenEndpoint(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class TokenEndpoint(BaseObj_v1_2):
     """ TokenEndpoint Object
     """
 
-    __swagger_fields__ = {
-        'url': None,
-        'tokenName': None,
+    __fields__ = {
+        'url': dict(builder=field, required=True),
+        'tokenName': dict(builder=field),
+    }
+
+    __renamed__ = {
+        'token_name': dict(key='tokenName'),
     }
 
 
-class AuthorizationCode(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class AuthorizationCode(BaseObj_v1_2):
     """ AuthorizationCode Object
     """
 
-    __swagger_fields__ = {
-        'tokenRequestEndpoint': None,
-        'tokenEndpoint': None,
+    __children__ = {
+        'tokenRequestEndpoint': dict(child_builder=TokenRequestEndpoint),
+        'tokenEndpoint': dict(child_builder=TokenEndpoint),
+    }
+
+    __renamed__ = {
+        'token_request_endpoint': dict(key='tokenRequestEndpoint'),
+        'token_endpoint': dict(key='tokenEndpoint'),
     }
 
 
-class GrantType(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
-    """ GrantType Object
+class GrantTypes(BaseObj_v1_2):
+    """ GrantTypes Object
     """
 
-    __swagger_fields__ = {
-        'implicit': None,
-        'authorization_code': None,
+    __children__ = {
+        'implicit': dict(child_builder=Implicit),
+        'authorization_code': dict(child_builder=AuthorizationCode),
     }
 
 
-class Authorizations(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Authorizations(BaseObj_v1_2):
     """ Authorizations Object
     """
 
-    __swagger_fields__ = {
-        'scope': None,
-        'description': None,
+    __fields__ = {
+        'scope': dict(builder=field),
+        'description': dict(builder=field),
     }
 
 
-class Authorization(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Authorization(BaseObj_v1_2):
     """ Authorization Object
     """
 
-    __swagger_fields__ = {
-        'type': None,
-        'passAs': None,
-        'keyname': None,
-        'scopes': None,
-        'grantTypes': None,
+    __fields__ = {
+        'type': dict(builder=field),
+        'passAs': dict(builder=field),
+        'keyname': dict(builder=field),
     }
 
-    def get_name(self, path):
-        return path.split('/', 3)[2]
+    __children__ = {
+        'scopes': dict(child_builder=list_(Scope)),
+        'grantTypes': dict(child_builder=GrantTypes),
+    }
+
+    __renamed__ = {
+        'type_': dict(key='type'),
+        'pass_as': dict(key='passAs'),
+        'grantTypes': dict(key='grant_types'),
+    }
 
 
-class ResponseMessage(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class ResponseMessage(BaseObj_v1_2):
     """ ResponseMessage Object
     """
 
-    __swagger_fields__ = {
-        'code': None,
-        'message': None,
-        'responseModel': None,
+    __fields__ = {
+        'code': dict(builder=field, required=True),
+        'message': dict(builder=field, required=True),
+        'responseModel': dict(builder=field),
+    }
+
+    __renamed__ = {
+        'response_model': dict(key='responseModel'),
     }
 
 
-class Parameter(six.with_metaclass(FieldMeta, DataTypeObj)):
+class Parameter(DataTypeObj):
     """ Parameter Object
     """
 
-    __swagger_fields__ = {
-        'paramType': None,
-        'name': None,
-        'required': None,
-        'allowMultiple': None,
-        'description': None,
+    __fields__ = {
+        'paramType': dict(builder=field, required=True),
+        'name': dict(builder=field, required=True),
+        'description': dict(builder=field),
+        'required': dict(builder=field),
+        'allowMultiple': dict(builder=field),
+    }
+
+    __renamed__ = {
+        'param_type': dict(key='paramType'),
+        'allow_multiple': dict(key='allowMultiple'),
     }
 
 
-class Operation(six.with_metaclass(FieldMeta, DataTypeObj)):
+class Operation(DataTypeObj):
     """ Operation Object
     """
 
-    __swagger_fields__ = {
-        'method': None,
-        'nickname': None,
-        'authorizations': None,
-        'parameters': None,
-        'responseMessages': None,
-        'produces': None,
-        'consumes': None,
-        'deprecated': None,
-        'summary': None,
-        'notes': None,
+    __fields__ = {
+        'method': dict(builder=field, required=True),
+        'summary': dict(builder=field),
+        'notes': dict(builder=field),
+        'nickname': dict(builder=field, required=True),
+        'produces': dict(builder=field),
+        'consumes': dict(builder=field),
+        'deprecated': dict(builder=field),
     }
 
-    __internal_fields__ = {
-        # path from Api object, concated with Resource object
-        'path': None,
+    __children__ = {
+        'authorizations': dict(child_builder=map_(list_(Authorizations))),
+        'parameters': dict(child_builder=list_(Parameter)),
+        'responseMessages': dict(child_builder=list_(ResponseMessage)),
     }
 
-    def get_name(self, path):
-        return self.nickname
+    __renamed__ = {
+        'response_messages': dict(key='responseMessages')
+    }
 
 
-class Api(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Api(BaseObj_v1_2):
     """ Api Object
     """
 
-    __swagger_fields__ = {
-        'path': None,
-        'operations': None,
-        'description': None,
+    __fields__ = {
+        'path': dict(builder=field, required=True),
+        'description': dict(builder=field),
+    }
+
+    __children__ = {
+        'operations': dict(child_builder=list_(Operation)),
     }
 
 
-class Property(six.with_metaclass(FieldMeta, DataTypeObj)):
+class Property(DataTypeObj):
     """ Property Object
     """
 
-    __swagger_fields__ = {
-        'description': None,
-    }     
+    __fields__ = {
+        'description': dict(builder=field),
+    }
 
 
-class Model(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Model(BaseObj_v1_2):
     """ Model Object
     """
 
-    __swagger_fields__ = {
-        'id': None,
-        'required': [],
-        'properties': None,
-        'subTypes': None,
-        'discriminator': None,
-        'description': None,
+    __fields__ = {
+        'id': dict(builder=field, required=True),
+        'description': dict(builder=field),
+        'required': dict(builder=field),
+        'subTypes': dict(builder=field),
+        'discriminator': dict(builder=field),
     }
 
-    __internal_fields__ = {
-        # for model inheritance
-        '_extends_': None,
+    __children__ = {
+        'properties': dict(child_builder=map_(Property), required=True),
     }
 
-    def get_name(self, path):
-        return self.id
+    __renamed__ = {
+        'id_': dict(key='id'),
+        'sub_types': dict(key='subTypes'),
+    }
 
 
-class Resource(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class ApiDeclaration(BaseObj_v1_2):
     """ Resource Object
+    The root object of each resource file
     """
 
-    __swagger_fields__ = {
-        'swaggerVersion': None,
-        'apiVersion': None,
-        'apis': None,
-        'basePath': None,
-        'resourcePath': None,
-        'models': None,
-        'produces': None,
-        'consumes': None,
-        'authorizations': None,
-        'description': None,
+    __fields__ = {
+        'swaggerVersion': dict(builder=field, required=True),
+        'apiVersion': dict(builder=field),
+        'basePath': dict(builder=field, required=True),
+        'resourcePath': dict(builder=field),
+        'produces': dict(builder=field),
+        'consumes': dict(builder=field),
+        'description': dict(builder=field),
     }
 
-    def __init__(self, ctx):
-        """ The original structure of API object is very bad
-        for seeking nickname for operations. Since nickname is unique
-        in one Resource, we can just make it flat.
-        """
-        super(Resource, self).__init__(ctx)
+    __children__ = {
+        'apis': dict(child_builder=list_(Api), required=True),
+        'models': dict(child_builder=map_(Model)),
+        'authorizations': dict(child_builder=map_(list_(Authorizations))),
+    }
 
-        new_api = {}
-        for api in ctx._obj['apis']:
-            for op in api.operations:
-                name = op.nickname
-                if name in new_api.keys():
-                    raise ValueError('duplication operation found: ' + name)
-
-                # Operation objects now have 'path' attribute.
-                op.update_field('path', api.path)
-                # Operation objects' parent is now Resource object(API Declaration).
-                op._parent__ = self
-                new_api[name] = op
-
-        # replace Api with Operations
-        self.update_field('apis', new_api)
-
-    def get_name(self, path):
-        return path.split('/', 3)[2]
+    __renamed__ = {
+        'swagger_version': dict(key='swaggerVersion'),
+        'api_version': dict(key='apiVersion'),
+        'base_path': dict(key='basePath'),
+        'resource_path': dict(key='resourcePath'),
+    }
 
 
-class Info(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class Info(BaseObj_v1_2):
     """ Info Object
     """
 
-    __swagger_fields__ = {
-        'title': None,
-        'termsOfServiceUrl': None,
-        'contact': None,
-        'license': None,
-        'licenseUrl': None,
-        'description': None,
+    __fields__ = {
+        'title': dict(builder=field, required=True),
+        'description': dict(builder=field, required=True),
+        'termsOfServiceUrl': dict(builder=field),
+        'contact': dict(builder=field),
+        'license': dict(builder=field),
+        'licenseUrl': dict(builder=field),
+    }
+
+    __renamed__ = {
+        'terms_of_service_url': dict(key='termsOfServiceUrl'),
+        'license_url': dict(key='licenseUrl'),
     }
 
 
-class ResourceList(six.with_metaclass(FieldMeta, BaseObj_v1_2)):
+class ResourceInListing(BaseObj_v1_2):
+    """ Resource object in "Resource Listing"
+    """
+    __fields__ = {
+        'path': dict(builder=field),
+        'description': dict(builder=field),
+    }
+
+
+class ResourceListing(BaseObj_v1_2):
     """ Resource List Object
     """
-    __swagger_fields__ = {
-        'swaggerVersion': None,
-        'apis': None,
-        'apiVersion': None,
-        'info': None,
-        'authorizations': None,
+    __fields__ = {
+        'swaggerVersion': dict(builder=field, required=True),
+        'apiVersion': dict(builder=field),
     }
 
+    __children__ = {
+        'apis': dict(child_builder=list_(ResourceInListing)),
+        'info': dict(child_builder=Info),
+        'authorizations': dict(child_builder=map_(Authorization)),
+    }
+
+    __renamed__ = {
+        'swagger_version': dict(key='swaggerVersion'),
+        'api_version': dict(key='apiVersion'),
+    }
+
+    __internal__ = {
+        'cached_apis': dict(),
+    }
